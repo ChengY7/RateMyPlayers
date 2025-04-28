802e91d441ed7a0af8956a6de991695a03dc3771
ChengY7 <Cheng91301@gmail.com>
2025-04-28 14:14:47 -0400

    First setup

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Player struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Active bool   `json:"active"`
+}
+
+func main() {
+	fmt.Println("Hello, World!!!!!")
+	app := fiber.New()
+
+	players := []Player{}
+
+	app.Get("/api/players", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(players)
+	})
+
+	app.Post("/api/players", func(c *fiber.Ctx) error {
+		player := &Player{}
+
+		if err := c.BodyParser(player); err != nil {
+			return err
+		}
+
+		if player.Name == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Name is required"})
+		}
+
+		player.ID = len(players) + 1
+		players = append(players, *player)
+		return c.Status(201).JSON(player)
+	})
+
+	app.Patch("/api/players/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, player := range players {
+			if fmt.Sprintf("%d", player.ID) == id {
+				players[i].Active = true
+				return c.Status(200).JSON(players[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Player not found"})
+	})
+
+	app.Delete("/api/players/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, player := range players {
+			if fmt.Sprintf("%d", player.ID) == id {
+				players = append(players[:i], players[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"message": "Player deleted"})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Player not found"})
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
