810fcd32fa84ae02b59a6ceb2eecda70577c53c0
ChengY7 <Cheng91301@gmail.com>
2025-04-28 15:14:13 -0400

    First setup

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Player struct {
@@ -16,6 +18,12 @@ type Player struct {
 func main() {
 	fmt.Println("Hello, World!!!!!")
 	app := fiber.New()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
 
 	players := []Player{}
 
@@ -65,5 +73,5 @@ func main() {
 		return c.Status(404).JSON(fiber.Map{"error": "Player not found"})
 	})
 
-	log.Fatal(app.Listen(":4000"))
+	log.Fatal(app.Listen(":" + PORT))
 }
